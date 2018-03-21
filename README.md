7dc17b8d0a82c452469c245febebc705660454f8
kdbeall <kdbeall@ncsu.edu>
2018-03-21 12:41:43 -0400

    added more reflection examples

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # java-concepts
+
+From [Stackoverflow](https://stackoverflow.com/questions/10546720/how-to-compile-packages-in-java) you can compile packages in Java as follows.
+
+Make a new directory called classes.
+
+When you are trying to compile the java class, use the '-d' option (destination) to specify where the .class files should be located.
+
+javac -d "classes" ./foodirectory/*.java
+
+and when you run your program, make sure that same folder is included in your class path:
+
+java -classpath "classes" foopackage.FooMainClass
+
+
--- reflection/Example.java
+++ reflection/Example.java
@@ -2,16 +2,43 @@ package reflection;
 
 import java.lang.reflect.*;
 
-public class Example{
+class Foo {
+    public int bar;
+    private String secret;
+    public Foo(){
+        this.bar = 1;
+        this.secret = "secret";
+    }
+    public void hello(){
+        System.out.println(Integer.toString(bar));
+    }
 
-    public static void main(String[] args){
-        try{
-            Object foo = Class.forName("Foo").newInstance();
-            Method m = foo.getClass().getDeclaredMethod("hello", new Class<?>[0]);
-            m.invoke(foo);
+}
 
-        }catch(Exception e){
-            System.out.println(e.getMessage());
+class Example{
+
+    public static void main(String[] args) throws Exception{
+
+        /** Retrieving Class Objects */
+        Class stringClass = "myString".getClass();
+        System.out.println(stringClass.toString());
+
+
+        /** Print all methods of a class */
+        Method[] methods = stringClass.getDeclaredMethods();
+        for(Method m : methods){
+            System.out.println(m.toString());
         }
+
+        /** Call a method using reflection */
+        Object foo = Class.forName("reflection.Foo").newInstance();
+        Method m = foo.getClass().getDeclaredMethod("hello", new Class<?>[0]);
+        m.invoke(foo);
+
+        /** Read a private variable using reflection */
+        Field field = Class.forName("reflection.Foo").getDeclaredField("secret");
+        field.setAccessible(true);
+        Object value = field.get(foo);
+        System.out.println((String) value);
     }
 }
